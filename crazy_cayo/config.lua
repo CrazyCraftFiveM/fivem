30435297823d55e6f923cfd41edbf51220c2c671
CrazyCraftFiveM <88507988+CrazyCraftFiveM@users.noreply.github.com>
2021-08-05 20:12:37 +0200

    Add files via upload
    Ceci est un script d'organisation sur cayo Perico

---
+++ crazy_cayo/config.lua
@@ -0,0 +1,32 @@
+Config             = {}
+
+Config.DrawDistance = 50
+Config.Size         = {x = 1.0, y = 1.0, z = 1.0}
+Config.Color        = {r = 125, g = 0, b = 0}
+Config.Type         = 20
+Config.EnableESXIdentity          = true 
+Config.EnableLicenses             = true
+
+Config.armurerie = {
+	{nom = "Pistolet", arme = "weapon_pistol", prix = 15000},
+	{nom = "SMG", arme = "weapon_smg", prix = 55000},
+	{nom = "Fusil à pompe", arme = "weapon_pumpshotgun", prix = 65000},
+}
+
+
+Config.pos = {
+	coffre = {
+		position = {x = 4965.76, y = -5786.09, z = 20.88}
+	},
+	garage = {
+		position = {x = 4982.89, y = -5730.39, z = 19.88}
+	},
+
+	spawnvoiture = {
+		position = {x = 4982.89, y = -5730.39, z = 19.88, h = 119.9}
+	},
+
+	armurerie = {
+		position = {x = 5011.72, y = -5744.96, z = 19.88}
+	}
+}
